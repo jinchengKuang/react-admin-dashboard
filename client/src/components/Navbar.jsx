134b8247bde419c1456215325b5a3e80a40d70a7
Jincheng Kuang <jkuang37@gatech.edu>
2023-02-15 14:32:03 -0500

    fix Navbar IconButton

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -19,7 +19,7 @@ import {
 } from '@mui/material';
 import profileImage from 'assets/profile.jpeg';
 
-const Navbar = () => {
+const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
 	const dispach = useDispatch();
 	const theme = useTheme();
 	return (
@@ -33,7 +33,7 @@ const Navbar = () => {
 			<Toolbar sx={{ justifyContent: 'space-between' }}>
 				{/* LEFT SIDE */}
 				<FlexBetween>
-					<IconButton onClick={() => console.log('open/close')}>
+					<IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
 						<MenuIcon />
 					</IconButton>
 					<FlexBetween
