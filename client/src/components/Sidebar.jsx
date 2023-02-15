36d787615c6d9e072d713310e9124efe672c7c52
Jincheng Kuang <jkuang37@gatech.edu>
2023-02-15 10:47:05 -0500

    continue setup frontend

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,196 @@
+import { useTheme } from '@emotion/react';
+import {
+	AdminPanelSettingsOutlined,
+	CalendarMonthOutlined,
+	ChevronLeft,
+	ChevronRightOutlined,
+	Groups2Outlined,
+	HomeOutlined,
+	PieChartOutline,
+	PieChartOutlined,
+	PointOfSaleOutlined,
+	PublicOutlined,
+	ReceiptLongOutlined,
+	ShoppingCartOutlined,
+	TodayOutlined,
+	TrendingFlatOutlined,
+} from '@mui/icons-material';
+import {
+	backdropClasses,
+	Drawer,
+	IconButton,
+	List,
+	ListItem,
+	ListItemButton,
+	ListItemText,
+	Typography,
+} from '@mui/material';
+import { Box, color } from '@mui/system';
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import FlexBetween from './FlexBetween';
+
+const navItems = [
+	{
+		text: 'Dashboard',
+		icon: <HomeOutlined />,
+	},
+	{
+		text: 'Client Facing',
+		icon: null,
+	},
+	{
+		text: 'Products',
+		icon: <ShoppingCartOutlined />,
+	},
+	{
+		text: 'Customers',
+		icon: <Groups2Outlined />,
+	},
+	{
+		text: 'Transaction',
+		icon: <ReceiptLongOutlined />,
+	},
+	{
+		text: 'Geography',
+		icon: <PublicOutlined />,
+	},
+	{
+		text: 'Sales',
+		icon: null,
+	},
+	{
+		text: 'Overview',
+		icon: <PointOfSaleOutlined />,
+	},
+	{
+		text: 'Daily',
+		icon: <TodayOutlined />,
+	},
+	{
+		text: 'Monthly',
+		icon: <CalendarMonthOutlined />,
+	},
+	{
+		text: 'Breakdown',
+		icon: <PieChartOutlined />,
+	},
+	{
+		text: 'Management',
+		icon: null,
+	},
+	{
+		text: 'Admin',
+		icon: <AdminPanelSettingsOutlined />,
+	},
+	{
+		text: 'Performance',
+		icon: <TrendingFlatOutlined />,
+	},
+];
+
+const Sidebar = ({
+	drawerWidth,
+	isSidebarOpen,
+	setIsSidebarOpen,
+	isNonMobile,
+}) => {
+	const { pathname } = useLocation;
+	const [active, setActive] = useState('');
+	const navigate = useNavigate();
+	const theme = useTheme();
+
+	useEffect(() => {
+		setActive(pathname.substring(1));
+	}, [pathname]);
+
+	return (
+		<Box component='nav'>
+			{isSidebarOpen && (
+				<Drawer
+					open={isSidebarOpen}
+					onClose={() => setIsSidebarOpen(false)}
+					variant='persistent'
+					anchor='left'
+					sx={{
+						width: drawerWidth,
+						'& .MuiDrawer-paper': {
+							color: theme.palette.secondary[200],
+							backgroundColor: theme.palette.background.alt,
+							boxSizing: 'border-box',
+							borderWidth: isNonMobile ? 0 : '2px',
+							width: drawerWidth,
+						},
+					}}
+				>
+					<Box width='100%'>
+						<Box m='1.5rem 2rem 2rem 3rem'>
+							<FlexBetween color={theme.palette.secondary.main}>
+								<Box display='flex' alignContent='center' gap='0.5rem'>
+									<Typography variant='h4' fontw='bold'>
+										ECOMVISION
+									</Typography>
+								</Box>
+								{!isNonMobile && (
+									<IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+										<ChevronLeft />
+									</IconButton>
+								)}
+							</FlexBetween>
+							<List>
+								{navItems.map(({ text, icon }) => {
+									if (!icon) {
+										return (
+											<Typography key={text} sx={{ m: '2.25rem 0 1rem 3rem' }}>
+												{text}
+											</Typography>
+										);
+									}
+									const lcText = text.toLowerCase();
+									return (
+										<List key={text} disablePadding>
+											<ListItemButton
+												onClick={() => {
+													navigate(`\${lcText}`);
+													setActive(lcText);
+												}}
+												sx={{
+													backgroundColor:
+														active === lcText
+															? theme.palette.secondary[300]
+															: 'transparent',
+													color:
+														active === lcText
+															? theme.palette.primary[600]
+															: theme.palette.secondary[100],
+												}}
+											>
+												<ListItemButton
+													sx={{
+														ml: '2rem',
+														color:
+															active === lcText
+																? theme.palette.primary[600]
+																: theme.palette.secondary[200],
+													}}
+												>
+													{icon}
+												</ListItemButton>
+												<ListItemText primary={text} />
+												{active === lcText && (
+													<ChevronRightOutlined sx={{ ml: 'auto' }} />
+												)}
+											</ListItemButton>
+										</List>
+									);
+								})}
+							</List>
+						</Box>
+					</Box>
+				</Drawer>
+			)}
+		</Box>
+	);
+};
+
+export default Sidebar;
--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -2,12 +2,24 @@ import { Box, useMediaQuery } from '@mui/material';
 import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from 'components/Navbar';
+import Sidebar from 'components/Sidebar';
 
 const Layout = () => {
+	const isNonMobile = useMediaQuery('(min-width: 60px)');
+	const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 	return (
-		<Box width='100%' height='100%'>
+		<Box display={isNonMobile ? 'flex' : 'block'} width='100%' height='100%'>
+			<Sidebar
+				isNonMobile={isNonMobile}
+				drawerWidth='250px'
+				isSidebarOpen={isSidebarOpen}
+				setIsSidebarOpen={setIsSidebarOpen}
+			></Sidebar>
 			<Box>
-				<Navbar />
+				<Navbar
+					isSidebarOpen={isSidebarOpen}
+					setIsSidebarOpen={setIsSidebarOpen}
+				/>
 				<Outlet />
 			</Box>
 		</Box>
