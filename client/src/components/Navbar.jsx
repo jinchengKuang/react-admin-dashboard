1effb29f22ecc8234a8ee57aca50306c12d9e4b4
Jincheng Kuang <jkuang37@gatech.edu>
2023-02-14 23:09:54 -0500

    setup client

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,70 @@
+import React, { Profiler, useState } from 'react';
+import {
+	LightModeOutlined,
+	DarkModeOutlined,
+	Menu as MenuIcon,
+	Search,
+	SettingsOutlined,
+	ArrowDropDownOutlined,
+} from '@mui/icons-material';
+import FlexBetween from './FlexBetween';
+import { useDispatch } from 'react-redux';
+import { setMode } from 'state';
+import {
+	AppBar,
+	IconButton,
+	InputBase,
+	Toolbar,
+	useTheme,
+} from '@mui/material';
+import profileImage from 'assests/profile.jpeg';
+
+const Navbar = () => {
+	const dispach = useDispatch();
+	const theme = useTheme();
+	return (
+		<AppBar
+			sx={{
+				position: 'static',
+				background: 'none',
+				boxShadow: 'none',
+			}}
+		>
+			<Toolbar sx={{ justifyContent: 'space-between' }}>
+				{/* LEFT SIDE */}
+				<FlexBetween>
+					<IconButton onClick={() => console.log('open/close')}>
+						<MenuIcon />
+					</IconButton>
+					<FlexBetween
+						backgroundColor={theme.palette.background.alt}
+						borderRadius='9px'
+						gap='3rem'
+						p='0.1rem 1.5rem'
+					>
+						<InputBase placeholder='Search...' />
+						<IconButton>
+							<Search />
+						</IconButton>
+					</FlexBetween>
+				</FlexBetween>
+
+				{/* RIGHT SIDE */}
+				<FlexBetween gap='1.5rem'>
+					<IconButton onClick={() => dispach(setMode())}>
+						{theme.palette.mode === 'dark' ? (
+							<DarkModeOutlined sx={{ fontSize: '25px' }} />
+						) : (
+							<LightModeOutlined sx={{ fontSize: '25px' }} />
+						)}
+					</IconButton>
+					<IconButton>
+						<SettingsOutlined />
+					</IconButton>
+				</FlexBetween>
+			</Toolbar>
+		</AppBar>
+	);
+};
+
+export default Navbar;
