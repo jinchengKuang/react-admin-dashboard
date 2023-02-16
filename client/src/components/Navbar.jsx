640a4f63559cccba6c1fde767cd44395975fd78b
Jincheng Kuang <jkuang37@gatech.edu>
2023-02-15 21:43:26 -0500

    setup frontend

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,16 +12,28 @@ import { useDispatch } from 'react-redux';
 import { setMode } from 'state';
 import {
 	AppBar,
+	Box,
+	Button,
 	IconButton,
 	InputBase,
+	Menu,
+	MenuItem,
 	Toolbar,
+	Typography,
 	useTheme,
 } from '@mui/material';
 import profileImage from 'assets/profile.jpeg';
 
-const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
+const Navbar = ({ user, isSidebarOpen, setIsSidebarOpen }) => {
 	const dispach = useDispatch();
 	const theme = useTheme();
+
+	const [anchorEl, setAnchorEl] = useState(null);
+
+	const isOpen = Boolean(anchorEl);
+	const handleClick = event => setAnchorEl(event.currentTarget);
+	const handleClose = () => setAnchorEl(null);
+
 	return (
 		<AppBar
 			sx={{
@@ -61,6 +73,55 @@ const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
 					<IconButton>
 						<SettingsOutlined />
 					</IconButton>
+
+					<FlexBetween>
+						<Button
+							onClick={handleClick}
+							sx={{
+								display: 'flex',
+								justifyContent: 'space-between',
+								alignItems: 'center',
+								textTransform: 'none',
+								gap: '1rem',
+							}}
+						>
+							<Box
+								component='img'
+								alt='profile'
+								src={profileImage}
+								height='32px'
+								width='32px'
+								borderRadius='50%'
+								sx={{ objectFit: 'cover' }}
+							/>
+							<Box textAlign='left'>
+								<Typography
+									fontWeight='bold'
+									fontSize='0.85rem'
+									sx={{ color: theme.palette.secondary[100] }}
+								>
+									{user.name}
+								</Typography>
+								<Typography
+									fontSize='0.75rem'
+									sx={{ color: theme.palette.secondary[200] }}
+								>
+									{user.occupation}
+								</Typography>
+							</Box>
+							<ArrowDropDownOutlined
+								sx={{ color: theme.palette.secondary[300], fontSize: '25px' }}
+							/>
+						</Button>
+						<Menu
+							anchorEl={anchorEl}
+							open={isOpen}
+							onClose={handleClose}
+							anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+						>
+							<MenuItem onClick={handleClose}>Log Out</MenuItem>
+						</Menu>
+					</FlexBetween>
 				</FlexBetween>
 			</Toolbar>
 		</AppBar>
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -11,6 +11,7 @@ import {
 	PointOfSaleOutlined,
 	PublicOutlined,
 	ReceiptLongOutlined,
+	SettingsOutlined,
 	ShoppingCartOutlined,
 	TodayOutlined,
 	TrendingFlatOutlined,
@@ -18,6 +19,7 @@ import {
 } from '@mui/icons-material';
 import {
 	backdropClasses,
+	Divider,
 	Drawer,
 	IconButton,
 	List,
@@ -31,6 +33,7 @@ import { Box, color } from '@mui/system';
 import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import FlexBetween from './FlexBetween';
+import profileImage from 'assets/profile.jpeg';
 
 const navItems = [
 	{
@@ -189,6 +192,42 @@ const Sidebar = ({
 								);
 							})}
 						</List>
+					</Box>
+
+					<Box position='absolute' bottom='2rem'>
+						<Divider />
+						<FlexBetween textTransform='none' gap='1rem' m='1.5rem 2rem 0 3rem'>
+							<Box
+								component='img'
+								alt='profile'
+								src={profileImage}
+								height='40px'
+								width='40px'
+								borderRadius='50%'
+								sx={{ objectFit: 'cover' }}
+							/>
+							<Box textAlign='left'>
+								<Typography
+									fontWeight='bold'
+									fontSize='0.9rem'
+									sx={{ color: theme.palette.secondary[100] }}
+								>
+									{user.name}
+								</Typography>
+								<Typography
+									fontSize='0.8rem'
+									sx={{ color: theme.palette.secondary[200] }}
+								>
+									{user.occupation}
+								</Typography>
+							</Box>
+							<SettingsOutlined
+								sx={{
+									color: theme.palette.secondary[300],
+									fontSize: '25px ',
+								}}
+							/>
+						</FlexBetween>
 					</Box>
 				</Drawer>
 			)}
